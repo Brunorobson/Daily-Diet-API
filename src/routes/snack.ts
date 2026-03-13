215733f8b0db1372e80a94255f5945dc9f27705c
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2026-03-13 19:02:33 -0300

    deletar refeicao

--- src/routes/snack.ts
+++ src/routes/snack.ts
@@ -33,4 +33,59 @@ export async function snackRoutes(app: FastifyInstance) {
       .status(201)
       .send({ message: 'Refeição salva com sucesso', user: snackBody })
   })
+
+  app.put('/:id', async (request, reply) => {
+    const getSnackBodySchema = z.object({
+      id: z.string().uuid(),
+    })
+    const updateSnackBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      dateTime: z.coerce.date(),
+      diet: z.boolean(),
+    })
+
+    const paramSnack = getSnackBodySchema.parse(request.params)
+    const bodySnack = updateSnackBodySchema.parse(request.body)
+
+    const sessionId = request.cookies.sessionId
+
+    if (sessionId !== paramSnack.id || !sessionId) {
+      return reply.status(404).send('Deu errado')
+    }
+    await knex('snacks').update({
+      name: bodySnack.name,
+      description: bodySnack.description,
+      dateTime: bodySnack.dateTime,
+      diet: bodySnack.diet,
+    })
+    return reply.status(201).send('Parabens cara')
+  })
+
+  app.delete('/delete/:id', async (request, reply) => {
+    const getSnackBodySchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const paramSnack = getSnackBodySchema.parse(request.params)
+
+    const sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      return reply.status(401).send('sem permissao')
+    }
+
+    const snack = await knex('snacks').where({ id: paramSnack.id }).first()
+
+    if (!snack) {
+      return reply.status(404).send('refeicao nao encontrada')
+    }
+
+    if (snack.userId !== sessionId) {
+      return reply.status(401).send('sem permissao')
+    }
+
+    await knex('snacks').where({ id: paramSnack.id }).delete()
+    return reply.status(204).send()
+  })
 }
